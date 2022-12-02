bbb3e904b5eb3d264682c7df18df1bbb115f47eb
Cesar de la Vega <cesarvegaro@gmail.com>
2022-12-02 16:18:12 +0100

    Move snippet to Android project for testing (#9)

---
+++ projects/android/app/src/main/java/com/example/docstesterapplication/MainApplicationOnlyPlayStore.kt
@@ -0,0 +1,14 @@
+package com.example.docstesterapplication
+
+import android.app.Application
+import com.revenuecat.purchases.Purchases
+import com.revenuecat.purchases.PurchasesConfiguration
+
+// If you're targeting only Google Play Store
+class MainApplicationOnlyPlayStore: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Purchases.debugLogsEnabled = true
+        Purchases.configure(PurchasesConfiguration.Builder(this, "public_google_sdk_key").build())
+    }
+}
