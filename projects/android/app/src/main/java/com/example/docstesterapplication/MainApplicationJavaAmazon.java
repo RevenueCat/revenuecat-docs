8e865ff7e1956cd35352a0dc9229d078e186d829
aboedo <andresboedo@gmail.com>
2022-12-05 19:11:08 -0300

    fixed bugs in android

--- projects/android/app/src/main/java/com/example/docstesterapplication/MainApplicationJavaAmazon.java
+++ projects/android/app/src/main/java/com/example/docstesterapplication/MainApplicationJavaAmazon.java
@@ -2,35 +2,24 @@ package com.example.docstesterapplication;
 
 import android.app.Application;
 
-import com.example.docstesterapplication.BuildConfig.STORE;
 import com.revenuecat.purchases.Purchases;
 import com.revenuecat.purchases.PurchasesConfiguration;
 import com.revenuecat.purchases.amazon.AmazonConfiguration;
-
-// If you're targeting only Google Play Store
-public class MainApplication extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Purchases.setDebugLogsEnabled(true);
-        Purchases.configure(new PurchasesConfiguration.Builder(this, "public_google_sdk_key").build());
-    }
-}
 
 // If you're building for the Amazon Appstore,
 // click the Kotlin tab to see how to set up flavors in your build.gradle:
 ///...
 
-public class MainApplication extends Application {
+public class MainApplicationJavaAmazon extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Purchases.setDebugLogsEnabled(true);
 
 
-        if (STORE.equals("amazon")) {
+        if (BuildConfig.STORE.equals("amazon")) {
             AmazonConfiguration.Builder builder = new AmazonConfiguration.Builder(this, "public_amazon_sdk_key");
-        } else if (STORE.equals("google")) {
+        } else if (BuildConfig.STORE.equals("google")) {
             PurchasesConfiguration.Builder builder = new PurchasesConfiguration.Builder(this, "public_google_sdk_key");
             Purchases.configure(builder.build());
         }
