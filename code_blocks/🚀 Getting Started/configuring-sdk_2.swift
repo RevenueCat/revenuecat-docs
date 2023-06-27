aabab8dfc64ab8b5153c5f27f78d11cf14df456b
NachoSoto <ignaciosoto90@gmail.com>
2023-06-27 07:24:02 -0700

    Configuring: consistent `SwiftUI` example
    Follow up to #136

--- code_blocks/🚀 Getting Started/configuring-sdk_2.swift
+++ code_blocks/🚀 Getting Started/configuring-sdk_2.swift
@@ -4,7 +4,7 @@ import Purchases
 struct SampleApp: App {
     init() {
         Purchases.logLevel = .debug
-        Purchases.configure(withAPIKey: "api_key")
+        Purchases.configure(withAPIKey: <public_apple_api_key>, appUserID: <app_user_id>)
     }
 
     var body: some Scene {
