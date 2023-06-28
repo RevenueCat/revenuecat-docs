9713a1ed78b9a1cc93f05d42347c1133303c51fa
NachoSoto <ignaciosoto90@gmail.com>
2023-06-28 12:50:59 -0700

    Reordered

--- code_blocks/👥 Customers/customer-info_1.swift
+++ code_blocks/👥 Customers/customer-info_1.swift
@@ -1,10 +1,10 @@
-// Using Completion Blocks
-Purchases.shared.getCustomerInfo { (customerInfo, error) in
-    // access latest customerInfo
-}
 // Using Swift Concurrency
 do {
     let customerInfo = try await Purchases.shared.customerInfo()
 } catch {
     // handle error
+}
+// Using Completion Blocks
+Purchases.shared.getCustomerInfo { (customerInfo, error) in
+    // access latest customerInfo
 }
--- code_blocks/🚀 Getting Started/getting-started_27.swift
+++ code_blocks/🚀 Getting Started/getting-started_27.swift
@@ -1,11 +1,11 @@
+// Using Swift Concurrency
+let customerInfo = try await Purchases.shared.customerInfo()
+if customerInfo.entitlements.all[<your_entitlement_id>]?.isActive == true {
+    // User is "premium"
+}
 // Using Completion Blocks
 Purchases.shared.getCustomerInfo { (customerInfo, error) in
     if customerInfo?.entitlements.all[<your_entitlement_id>]?.isActive == true {
         // User is "premium"
     }
-}
-// Using Swift Concurrency
-let customerInfo = try await Purchases.shared.customerInfo()
-if customerInfo.entitlements.all[<your_entitlement_id>]?.isActive == true {
-    // User is "premium"
 }
