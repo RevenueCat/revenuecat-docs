d0ac089793e823c5c05ea41cf4262376e1feb2df
NachoSoto <ignaciosoto90@gmail.com>
2023-06-28 08:36:51 -0700

    `Swift`: add `async` version of `getCustomerInfo()`
    Follow up to #137.
    There's a few more examples, but these are the main ones.

--- code_blocks/🚀 Getting Started/getting-started_27.swift
+++ code_blocks/🚀 Getting Started/getting-started_27.swift
@@ -1,5 +1,11 @@
+// Using Completion Blocks
 Purchases.shared.getCustomerInfo { (customerInfo, error) in
     if customerInfo?.entitlements.all[<your_entitlement_id>]?.isActive == true {
         // User is "premium"
     }
+}
+// Using Swift Concurrency
+let customerInfo = try await Purchases.shared.customerInfo()
+if customerInfo.entitlements.all[<your_entitlement_id>]?.isActive == true {
+    // User is "premium"
 }
