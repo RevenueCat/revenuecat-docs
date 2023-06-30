71c895b1e1b795f841aee256d2dfa4208237d845
NachoSoto <ignaciosoto90@gmail.com>
2023-06-30 13:11:00 -0700

    Listening for `CustomerInfo` updates: added `AsyncStream` example
    Updates https://www.revenuecat.com/docs/customer-info#listening-for-customerinfo-updates

--- code_blocks/👥 Customers/customer-info_25.swift
+++ code_blocks/👥 Customers/customer-info_25.swift
@@ -1,13 +1,15 @@
-// Additional configure setup
-func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-  
-    Purchases.logLevel = .debug
-    Purchases.configure(withAPIKey: <public_sdk_key>)
-    Purchases.shared.delegate = self // make sure to set this after calling configure
-}
+// Using PurchasesDelegate:
+Purchases.logLevel = .debug
+Purchases.configure(withAPIKey: <public_sdk_key>)
+Purchases.shared.delegate = self // make sure to set this after calling configure
 
 extension AppDelegate: PurchasesDelegate {
     func purchases(_ purchases: Purchases, receivedUpdated customerInfo: Purchases.CustomerInfo) {
         // handle any changes to customerInfo
     }
+}
+
+// Using Swift Concurrency:
+for try await customerInfo in Purchases.shared.customerInfoStream {
+    // handle any changes to customerInfo
 }
