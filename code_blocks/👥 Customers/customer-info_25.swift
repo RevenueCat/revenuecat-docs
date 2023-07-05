6b171d476c679c92da6db9af4a5167c56531acee
NachoSoto <ignaciosoto90@gmail.com>
2023-07-05 14:46:13 -0700

    Added comments

--- code_blocks/👥 Customers/customer-info_25.swift
+++ code_blocks/👥 Customers/customer-info_25.swift
@@ -1,4 +1,4 @@
-// Using PurchasesDelegate:
+// Option 1: using PurchasesDelegate:
 Purchases.logLevel = .debug
 Purchases.configure(withAPIKey: <public_sdk_key>)
 Purchases.shared.delegate = self // make sure to set this after calling configure
@@ -9,7 +9,7 @@ extension AppDelegate: PurchasesDelegate {
     }
 }
 
-// Using Swift Concurrency:
+// Option 2: using Swift Concurrency:
 for try await customerInfo in Purchases.shared.customerInfoStream {
     // handle any changes to customerInfo
 }
