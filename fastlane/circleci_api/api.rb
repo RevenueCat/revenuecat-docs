9fbe3cd4c3c1245816b286b4ced144f71b3433a8
Cesar de la Vega <cesarvegaro@gmail.com>
2023-07-10 17:25:07 +0200

    Added previewing of docs

---
+++ fastlane/circleci_api/api.rb
@@ -0,0 +1,17 @@
+UI = Fastlane::UI
+
+def get_artifacts(job_id, circleci_token)
+    url = URI.parse("https://circleci.com/api/v2/project/gh/RevenueCat/revenuecat-docs/#{job_id}/artifacts")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+
+    request = Net::HTTP::Get.new(url.request_uri)
+    request.basic_auth(circleci_token, '')
+
+    response = http.request(request)
+    unless response.is_a?(Net::HTTPSuccess)
+        UI.user_error!("Error retrieving artifacts.\nCode: #{response.code}\nBody: #{response.read_body}")
+    end
+
+    JSON.parse(response.body)["items"]
+end
---
+++ fastlane/rendering.rb
@@ -0,0 +1,44 @@
+import 'files.rb'
+require_relative 'circleci_api/api.rb'
+require_relative 'github_api/api.rb'
+
+UI = Fastlane::UI
+
+def post_circleci_artifacts(job_id, circleci_token, github_token)
+    UI.message("Posting artifacts for job #{job_id}")
+
+    artifacts = get_artifacts(job_id, circleci_token)
+
+    unless artifacts.length > 0
+        UI.message("No artifacts found for job #{job_id}")
+        return
+    end
+
+    comment = build_pr_comment(artifacts)
+
+    comment_url = get_pr_comment_url(github_token, ENV['CIRCLE_PROJECT_USERNAME'], ENV['CIRCLE_BRANCH'])
+    all_comments = get_pr_comments(comment_url, github_token)
+
+    rcgitbot_comment = all_comments.find { |item| item["user"]["login"] == 'RCGitBot' }
+    if rcgitbot_comment
+        delete_comment(rcgitbot_comment['url'], github_token)
+    end
+
+    post_new_comment(comment_url, comment, github_token)
+end
+
+def build_pr_comment(artifacts)
+    comment_lines = []
+    comment_lines << "## Previews"
+
+    artifacts.each do |artifact|
+        path = artifact['path']
+        url = artifact['url']
+        comment_lines << "[#{path}](#{url})"
+        comment_lines << "<details><summary>See contents</summary>"
+        comment_lines << get_doc_content(path)
+        comment_lines << "</details>\n"
+    end
+
+    "#{comment_lines.join("\n")}"
+end
