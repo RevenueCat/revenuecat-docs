72b22097b3d1d8ec5c90a10f7334f90782a9f02e
Cesar de la Vega <cesarvegaro@gmail.com>
2023-07-27 17:12:02 +0200

    indentation fix

--- spec/test_files/code_blocks/➡️ Migrating To RevenueCat/observer-mode_1.swift
+++ spec/test_files/code_blocks/➡️ Migrating To RevenueCat/observer-mode_1.swift
@@ -1,10 +1,10 @@
 Whatever other code we don't care about
-// MARK: Observer mode configuration
-Purchases.logLevel = .debug
-Purchases.configure(
-  with: Configuration.Builder(withAPIKey: Constants.apiKey)
-    .with(appUserID: <app_user_id>)
-    .with(observerMode: true)
-    .build()
-)
-// END
+    // MARK: Observer mode configuration
+    Purchases.logLevel = .debug
+    Purchases.configure(
+      with: Configuration.Builder(withAPIKey: Constants.apiKey)
+        .with(appUserID: <app_user_id>)
+        .with(observerMode: true)
+        .build()
+    )
+    // END
