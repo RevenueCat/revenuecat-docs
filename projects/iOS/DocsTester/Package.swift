7049d2bd8be8b67917d7c1e08beeebb2f1e6dfd6
Cesar de la Vega <cesarvegaro@gmail.com>
2023-08-04 13:06:48 +0200

    Add swiftlint to iOS project (#231)

--- projects/iOS/DocsTester/Package.swift
+++ projects/iOS/DocsTester/Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "DocsTester",
+    platforms: [.macOS(.v12)],
     products: [
         .library(
             name: "DocsTester",
@@ -11,12 +12,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/revenuecat/purchases-ios", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/realm/SwiftLint", .upToNextMajor(from: "0.52.4")),
     ],
     targets: [
         .target(
             name: "DocsTester",
             dependencies: [
                 .product(name: "RevenueCat", package: "purchases-ios"),
-            ])
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-warnings-as-errors"])
+            ],
+            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+        )
     ]
 )
--- projects/iOS/DocsTester/Sources/DocsTester/Constants.swift
+++ projects/iOS/DocsTester/Sources/DocsTester/Constants.swift
@@ -4,7 +4,7 @@ import Foundation
  Configuration file for your app's RevenueCat settings.
  */
 
-struct Constants {
+enum Constants {
 
     /*
      The API key for your app from the RevenueCat dashboard: https://app.revenuecat.com
@@ -12,7 +12,8 @@ struct Constants {
     static let apiKey = "REVENUECAT_API_KEY"
 
     /*
-     The entitlement ID from the RevenueCat dashboard that is activated upon successful in-app purchase for the duration of the purchase.
+     The entitlement ID from the RevenueCat dashboard that is activated upon successful
+     in-app purchase for the duration of the purchase.
      */
     static let entitlementID = "premium"
 
