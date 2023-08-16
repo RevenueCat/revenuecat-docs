8a22c74cf3949a37fce565198d6cb3c531a11d6d
Cesar de la Vega <cesarvegaro@gmail.com>
2023-08-16 13:48:54 +0200

    fix comments indentation

--- code_blocks/👥 Customers/verification-result.swift
+++ code_blocks/👥 Customers/verification-result.swift
@@ -1,23 +1,23 @@
 let user = try await Purchases.shared.customerInfo()
 
 switch user.entitlements.verificationResult {
-    /// No verification was done.
-    ///
-    /// This can happen for multiple reasons:
-    ///  1. Verification is not enabled in ``Configuration``
-    ///  2. Verification can't be performed prior to iOS 13.0
+/// No verification was done.
+///
+/// This can happen for multiple reasons:
+///  1. Verification is not enabled in ``Configuration``
+///  2. Verification can't be performed prior to iOS 13.0
 case .notRequested:
     break
 
-    /// Entitlements were verified with our server.
+/// Entitlements were verified with our server.
 case .verified:
     break
 
-    /// Entitlements were created and verified on device through `StoreKit 2`.
+/// Entitlements were created and verified on device through `StoreKit 2`.
 case .verifiedOnDevice:
     break
 
-    /// Entitlement verification failed, possibly due to a MiTM attack.
+/// Entitlement verification failed, possibly due to a MiTM attack.
 case .failed:
     break
 }
