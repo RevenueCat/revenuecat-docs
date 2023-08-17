98ea3a94acdc00a4c67420e6a6685e435c62c718
Cesar de la Vega <cesarvegaro@gmail.com>
2023-08-17 17:25:37 +0200

    collect errors

--- fastlane/checks.rb
+++ fastlane/checks.rb
@@ -1,13 +1,19 @@
 def check_links_in_folder(folder)
+    all_errors = []
     markdown_files(folder).each do |markdown_file|
-        check_links_in_file(markdown_file)
+        errors = check_links_in_file(markdown_file)
+        errors.each do |error|
+            all_errors << error unless all_errors.include?(error)  # Add error if not already present
+        end
     end
+    return all_errors
 end
 
 def check_links_in_file(markdown_file)
     markdown_content = get_file_contents(markdown_file)
     links = markdown_content.scan(/\[([^\]]+)\]\(([^)]+)\)/)
 
+    errors = []
     links.each do |text, url|
         next unless url.start_with?('http')
 
@@ -19,6 +25,7 @@ def check_links_in_file(markdown_file)
             Fastlane::UI.message("OK - #{url}") if response.status[0] == '200'
         rescue OpenURI::HTTPError => e
             Fastlane::UI.error("Broken link in #{markdown_file}: #{url} - #{e.message}")
+            errors << url
         rescue StandardError => e
             Fastlane::UI.message("Error checking link: #{url} - #{e.message}")
         end
