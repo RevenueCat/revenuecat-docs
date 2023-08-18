f72e5f122d32d29b78d9cdf71571d8c953046220
Toni Rico <toni.rico.diez@revenuecat.com>
2023-08-18 08:59:20 +0200

    Update sample dependencies versions

--- code_blocks/🧰 Test - Launch/debugging_13.groovy
+++ code_blocks/🧰 Test - Launch/debugging_13.groovy
@@ -1,2 +1,2 @@
-debugImplementation "com.revenuecat.purchase:purchases-debugview:6.10.0"
-releaseImplementation "com.revenuecat.purchase:purchases-debugview-noop:6.10.0"
+debugImplementation "com.revenuecat.purchase:purchases-debugview:6.9.2"
+releaseImplementation "com.revenuecat.purchase:purchases-debugview-noop:6.9.2"
