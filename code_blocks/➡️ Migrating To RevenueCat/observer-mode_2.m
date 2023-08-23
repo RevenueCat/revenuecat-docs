b053ad77866a663ecae53abe6f6ca9ac51f9bec9
NachoSoto <ignaciosoto90@gmail.com>
2023-08-23 12:17:02 -0700

    Using new API from https://github.com/RevenueCat/purchases-ios/pull/3066

--- code_blocks/➡️ Migrating To RevenueCat/observer-mode_2.m
+++ code_blocks/➡️ Migrating To RevenueCat/observer-mode_2.m
@@ -1,7 +1,6 @@
 RCPurchases.logLevel = RCLogLevelDebug;
 RCConfigurationBuilder *configuration = [RCConfiguration builderWithAPIKey:@<public_sdk_key>];
-configuration = [configuration withObserverMode:YES];
-configuration = [configuration withAppUserID:@<app_user_id>];
 // If your app uses StoreKit 2, you must enable it in the SDK
-configuration = [configuration withUsesStoreKit2IfAvailable:YES];
+configuration = [configuration withObserverMode:YES, storeKit2:NO];
+configuration = [configuration withAppUserID:@<app_user_id>];
 [RCPurchases configureWithConfiguration:[configuration build]];
--- projects/iOS/DocsTester/Sources/DocsTester/DocsTester.swift
+++ projects/iOS/DocsTester/Sources/DocsTester/DocsTester.swift
@@ -7,11 +7,10 @@ final class Container {
         Purchases.logLevel = .debug
         Purchases.configure(
             with: Configuration.Builder(withAPIKey: Constants.apiKey)
-                .with(observerMode: true)
+                // If your app uses StoreKit 2, you must enable it in the SDK
+                .with(observerMode: true, storeKit2: false)
                 // Optionally set an app user ID for the user
                 .with(appUserID: UserManager.appUserID)
-                // If your app uses StoreKit 2, you must enable it in the SDK
-                .with(usesStoreKit2IfAvailable: true)
                 .build()
         )
         // END
