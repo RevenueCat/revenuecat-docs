6924500990332b87fd5b878cd9470df7d6314415
Josh Holtz <me@joshholtz.com>
2023-08-24 13:28:03 -0500

    Update code_blocks/🚀 Getting Started/displaying-products/paywalls/paywalls_6.swift
    Co-authored-by: NachoSoto <NachoSoto@users.noreply.github.com>

--- code_blocks/🚀 Getting Started/displaying-products/paywalls/paywalls_6.swift
+++ code_blocks/🚀 Getting Started/displaying-products/paywalls/paywalls_6.swift
@@ -10,7 +10,8 @@ struct YourPaywall: View {
     var body: some View {
         ScrollView {
             // Your custom paywall design content
-        }.paywallFooter(offering: offering, condensed: true) { customerInfo in
+        }
+        .paywallFooter(offering: offering, condensed: true) { customerInfo in
             // Purchase completed! Thank your user and dismiss your paywall
         }
     }
