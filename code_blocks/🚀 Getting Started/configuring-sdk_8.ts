7d838d4132603e54aa89fbcc33216ae6b5e2f0f4
Toni Rico <antonio.rico.diez@revenuecat.com>
2023-08-31 13:32:20 +0200

    Fix configuring capacitor docs

--- code_blocks/🚀 Getting Started/configuring-sdk_8.ts
+++ code_blocks/🚀 Getting Started/configuring-sdk_8.ts
@@ -1,10 +1,8 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-
-async function onDeviceReady() {
+const onDeviceReady = async () => {
     await Purchases.setLogLevel(LOG_LEVEL.DEBUG);
-    if (window.cordova.platformId === 'ios') {
+    if (Capacitor.getPlatform() === 'ios') {
         await Purchases.configure({ apiKey: <public_apple_api_key> });
-    } else if (window.cordova.platformId === 'android') {
+    } else if (Capacitor.getPlatform() === 'android') {
         await Purchases.configure({ apiKey: <public_google_api_key> });
     }
     // OR: if building for Amazon, be sure to follow the installation instructions then:
