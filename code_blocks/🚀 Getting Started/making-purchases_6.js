a941d6fbbd5e0d5fd2e666098f4019c9c6586fa8
Cesar de la Vega <cesarvegaro@gmail.com>
2023-12-26 16:25:06 +0100

    fix react native code

--- code_blocks/🚀 Getting Started/making-purchases_6.js
+++ code_blocks/🚀 Getting Started/making-purchases_6.js
@@ -11,9 +11,9 @@ try {
 }
 
 // -----
-// If you are NOT using Offerings/Packages:
-await Purchases.purchaseProduct("product_id");
-
-// Or, optionally provide the product type as the third parameter. Defaults to PURCHASE_TYPE.SUBS
-// The `null` second parameter is the `upgradeInfo` object discussed here: https://www.revenuecat.com/docs/managing-subscriptions#google-play
-await Purchases.purchaseProduct("product_id", null, Purchases.PURCHASE_TYPE.INAPP);
+// If you are NOT using Offerings/Packages, first fetch the store products
+const products: PurchasesStoreProduct[] = await Purchases.getProducts(
+    ["product_id"],
+    PURCHASE_TYPE.INAPP
+);
+await Purchases.purchaseStoreProduct(products[0]);
