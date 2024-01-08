92656d48dd84fe36c1a812bf12a731451517c129
Toni Rico <toni.rico.diez@revenuecat.com>
2024-01-08 09:15:59 +0100

    Update paywalls activity java sample with better instructions (#625)
    ## Motivation / Description
    Improves docs added in #621 
    
    Adds the way to launch the paywall and some instructions on the
    available methods for it.

--- code_blocks/🛠 Tools/paywalls/paywalls_activity_java.java
+++ code_blocks/🛠 Tools/paywalls/paywalls_activity_java.java
@@ -1,11 +1,19 @@
 @OptIn(markerClass = ExperimentalPreviewRevenueCatUIPurchasesAPI.class)
 public class MainActivity extends AppCompatActivity implements PaywallResultHandler {
     private PaywallActivityLauncher launcher;
+    private static final String requiredEntitlementIdentifier = "MY_ENTITLEMENT_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         launcher = new PaywallActivityLauncher(this, this);
+    }
+
+    private void launchPaywallActivity() {
+        // This will launch the paywall only if the user doesn't have the given entitlement id active.
+        launcher.launchIfNeeded(requiredEntitlementIdentifier);
+        // or if you want to launch it without any conditions
+        launcher.launch();
     }
 
     @Override
