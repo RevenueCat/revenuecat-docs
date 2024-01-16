def260fb2d0dc6d64149560ba0fea483c38f32cf
NachoSoto <ignaciosoto90@gmail.com>
2024-01-16 08:34:06 -0800

    RN Paywalls: simplify code samples

--- code_blocks/🛠 Tools/paywalls/paywalls_rn_3.tsx
+++ code_blocks/🛠 Tools/paywalls/paywalls_rn_3.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
+import { Text } from 'react-native';
+
 import RevenueCatUI from 'react-native-purchases-ui';
 
-import { Text } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import RootStackParamList from '../RootStackParamList';
-
-type Props = NativeStackScreenProps<RootStackParamList, 'FooterPaywall'>;
-
-const FooterPaywallScreen: React.FC<Props> = () => {
-    return (
-        <RevenueCatUI.PaywallFooterContainerView style={{ backgroundColor: '#f8f8f8' }}>
-            <Text>
-                Your Custom Paywall Design
-            </Text>
-        </RevenueCatUI.PaywallFooterContainerView>
-    );
-};
+return (
+    <RevenueCatUI.PaywallFooterContainerView style={{ backgroundColor: '#f8f8f8' }}>
+        <Text>
+            Your Custom Paywall Design
+        </Text>
+    </RevenueCatUI.PaywallFooterContainerView>
+);
