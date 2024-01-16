9e2857882afbe4b6cd2881424b7ff47c29229f0c
NachoSoto <NachoSoto@users.noreply.github.com>
2024-01-16 10:18:05 -0800

    RN Paywalls: simplify code samples (#643)

--- code_blocks/🛠 Tools/paywalls/paywalls_rn_2.tsx
+++ code_blocks/🛠 Tools/paywalls/paywalls_rn_2.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
+import { View } from 'react-native';
+
 import RevenueCatUI from 'react-native-purchases-ui';
 
-import { StyleSheet, View } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import RootStackParamList from '../RootStackParamList';
-
-type Props = NativeStackScreenProps<RootStackParamList, 'Paywall'>;
-
-const PaywallScreen: React.FC<Props> = () => {
-    const styles = StyleSheet.create({
-        flex1: {
-            flex: 1,
-        },
-    });
-
-    return (
-        <View style={styles.flex1}>
-            <RevenueCatUI.Paywall />
-        </View>
-    );
-};
+return (
+    <View style={{ flex: 1 }}>
+        <RevenueCatUI.Paywall />
+    </View>
+);
