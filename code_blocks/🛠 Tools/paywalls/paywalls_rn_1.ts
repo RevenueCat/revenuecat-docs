60717faa20101aa48e76d2d1af87fec022d00a04
Cesar de la Vega <cesarvegaro@gmail.com>
2024-01-23 11:41:47 +0100

    Update paywalls_rn_1.ts

--- code_blocks/🛠 Tools/paywalls/paywalls_rn_1.ts
+++ code_blocks/🛠 Tools/paywalls/paywalls_rn_1.ts
@@ -1,4 +1,4 @@
-import RevenueCatUI, { PAYWALL_RESULT } from "../react-native-purchases-ui";
+import RevenueCatUI, { PAYWALL_RESULT } from "react-native-purchases-ui";
 
 async function presentPaywall(): Promise<boolean> {
     const paywallResult: PAYWALL_RESULT = await RevenueCatUI.presentPaywall();
