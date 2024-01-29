6fe2b2769c1624f9e47156b1c038d42b0d8771e2
NachoSoto <ignaciosoto90@gmail.com>
2024-01-29 11:52:05 -0800

    `RNPaywalls`: add offering parameters
    Depends on https://github.com/RevenueCat/react-native-purchases/pull/850

--- code_blocks/🛠 Tools/paywalls/paywalls_rn_3.tsx
+++ code_blocks/🛠 Tools/paywalls/paywalls_rn_3.tsx
@@ -4,7 +4,12 @@ import { Text } from 'react-native';
 import RevenueCatUI from 'react-native-purchases-ui';
 
 return (
-    <RevenueCatUI.PaywallFooterContainerView style={{ backgroundColor: '#f8f8f8' }}>
+    <RevenueCatUI.PaywallFooterContainerView
+        style={{ backgroundColor: '#f8f8f8' }}
+        options={{
+            offering: offering // Optional offering parameter
+        }}
+    >
         <Text>
             Your Custom Paywall Design
         </Text>
